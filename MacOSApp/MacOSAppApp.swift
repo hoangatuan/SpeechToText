298c3d5c8b837ea2d38c00aa5e61de1c5bbbf4dd
tuan.hoang <ext.tuan.hoang@grabtaxi.com>
2023-05-01 23:00:54 +0700

    Create project

---
+++ MacOSApp/MacOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOSAppApp.swift
+//  MacOSApp
+//
+//  Created by Tuan Hoang on 20/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MacOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
